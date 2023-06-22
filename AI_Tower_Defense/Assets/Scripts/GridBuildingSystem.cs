73ef5416418a041ba05e53bd95f0e542f0b6ac5c
Stefan <stmepp@online.de>
2023-06-22 17:11:55 +0200

    Added Buiding SpawnLogic into GridBuilding System
    can create Towers now, but still needs reference

--- AI_Tower_Defense/Assets/Scripts/GridBuildingSystem.cs
+++ AI_Tower_Defense/Assets/Scripts/GridBuildingSystem.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class GridBuildingSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private Transform testTowerTransform;
+
+    private Grid<GridObject> grid;
+
+    private void Awake()
     {
-        
+        int gridWidth = 10;
+        int gridHeight = 10;
+        float cellSize = 10f;
+        grid = new Grid<GridObject>(gridWidth, gridHeight, cellSize, Vector3.zero, (Grid<GridObject> g, int x, int z) => new GridObject(g, x, z));
+    }
+    public class GridObject
+    {
+        private Grid<GridObject> grid;
+        private int x;
+        private int z;
+
+        public GridObject(Grid<GridObject> grid, int x, int z)
+        {
+            this.grid = grid;
+            this.x = x;
+            this.z = z;
+        }
+
+        public override string ToString()
+        {
+            return x + " , " + z;
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            grid.GetXZ(Mouse3D.GetMouseWorldPosition(), out int x, out int z);
+            Instantiate(testTowerTransform, grid.GetWorldPosition(x, z), Quaternion.identity);
+        }
     }
 }
