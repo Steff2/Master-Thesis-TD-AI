362363af977c754f25d5725f94ae36f638103af2
Stefan <stmepp@online.de>
2023-06-23 16:18:07 +0200

    Unity backend

--- AI_Tower_Defense/Assets/Scripts/TowerTypeSO.cs
+++ AI_Tower_Defense/Assets/Scripts/TowerTypeSO.cs
@@ -5,8 +5,18 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "TowerTypeScriptableObject", menuName = "ScriptableObjects/TowerTypeScriptableObject")]
 public class TowerTypeSO : ScriptableObject
 {
+    public enum DamageType
+    {
+        SingleTarget,
+        AreaDamage
+    }
+
     public string nameString;
     public Transform prefab;
+    public Transform visuals;
     public int width;
     public int height;
+    public int damage;
+    public Transform projectile;
+    public DamageType damageType;
 }
