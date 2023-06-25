cea6e0c267b176a07c198d2f68d895e25108f266
Stefan <stmepp@online.de>
2023-06-25 18:52:18 +0200

    Adjusted Arrow rotation by X = 90 degrees

--- AI_Tower_Defense/Assets/Scripts/ProjectileArrow.cs
+++ AI_Tower_Defense/Assets/Scripts/ProjectileArrow.cs
@@ -6,14 +6,14 @@ public class ProjectileArrow : MonoBehaviour
 {
     public static void Create(TowerTypeSO towerType, Vector3 spawnPosition, Vector3 targetPosition)
     {
-        Transform arrowTransform = Instantiate(towerType.projectile, spawnPosition, Quaternion.identity);
+        Transform arrowTransform = Instantiate(towerType.projectile, spawnPosition, Quaternion.Euler(90, 0, 0));
 
         ProjectileArrow projectileArrow = arrowTransform.GetComponent<ProjectileArrow>();
         projectileArrow.Setup(targetPosition);
     }
 
     private Vector3 targetPosition;
-    private float moveSpeed = 60f;
+    private float moveSpeed = 10f;
     //private float turnSpeed = 100f;
     private float destroySelfDistance = 1f;
     private void Setup(Vector3 targetPosition) 
@@ -27,14 +27,15 @@ public class ProjectileArrow : MonoBehaviour
 
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
-
+        transform.LookAt(targetPosition);
+        transform.eulerAngles += new Vector3(90, 0, 0);
         //var angle = Vector3.Angle(transform.position, moveDir);
 
         //transform.eulerAngles = new Vector3(angle, angle, angle);
         /*float angle = Mathf.Atan2(moveDir.y, moveDir.x) * Mathf.Rad2Deg;
         if (angle < 0) angle += 360;
 
-        transform.eulerAngles = new Vector3(0, angle, angle);*/
+        transform.eulerAngles = new Vector3(90, angle, angle);*/
 
         if (Vector3.Distance(transform.position, targetPosition) < destroySelfDistance)
         {
